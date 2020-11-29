5fd6f466bafdefc7f46f2dce9f26afbe6f1d194d
yRxf <524186662@qq.com>
2020-11-29 13:55:18 +0800

    修复网易云音乐获取歌曲名字错误bug

--- dwmbar-functions/dwm_CloudMusic_Song.sh
+++ dwmbar-functions/dwm_CloudMusic_Song.sh
@@ -5,7 +5,7 @@
 dwm_CloudMusic_Song () {
   if [ $(ps -ef | grep -c netease-cloud-music) -gt 1 ]; then
     Song=$(awk -F '"track":{' '{print $2}' ~/.cache/netease-cloud-music/StorageCache/webdata/file/history)
-    Songname=$(echo $Song | awk -F , '{print $1}' | sed s/\"//g | awk -F : '{print $2}')
+    Songname=$(echo $Song | awk -F "commentThreadId" '{print $2}' | awk -F "name" '{print $2}' | awk -F , '{print $1}' | sed s/\"//g | awk -F : '{print $2}')
     Songer=$(echo $Song | awk -F '"artists":' '{print $2}' | awk -F '}]' '{print $1}' | sed s/\"//g | awk -F 'name:' '{i=2;while (i<=NF) {print $i; i++}}' | awk -F , '{print $1}' | tr "\n" "\/" |sed -e 's/\/$/\n/')
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
